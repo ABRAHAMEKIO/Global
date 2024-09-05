e6d5e2e5560fb9e66a139d25f2d0dfe230d66dca
unknown <ekioabraham570@gmail.com>
2024-09-05 15:34:02 +0100

    added explore page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Home from './Pages/Home'
 import ConnectWallet from './Pages/ConnectWallet'
 import Create from './Pages/Create'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Explore from './Pages/Explore'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/Connectwallet' element={<ConnectWallet/>}/>
         <Route path='/create' element={<Create/>}/>
+        <Route path='/Explore' element={<Create/>}/>
       </Routes>
       </BrowserRouter>
       <Footer/>
